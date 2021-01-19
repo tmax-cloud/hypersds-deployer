57d4f8751ff1c2c4cbaaedceb761db1827bf3dd2
donggyu_park <donggyu_park@tmax.co.kr>
2021-01-19 17:44:57 +0900

    Refactoring util to TDD base and Add TDD sample

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module hypersds-provisioner
 
 go 1.13
+
+require (
+	github.com/golang/mock v1.4.4
+	github.com/onsi/ginkgo v1.14.2
+	github.com/onsi/gomega v1.10.4
+)
--- pkg/util/ssh.go
+++ pkg/util/ssh.go
@@ -1,17 +1,21 @@
 package util
 
 import (
-	"os/exec"
-	"time"
 	"bytes"
 	"context"
+	"time"
 )
 
 const (
-	cmdTimeout = 20*time.Minute
+	cmdTimeout = 20 * time.Minute
 )
 
-func RunSSHCmd(hostName, hostAddr string, cephQuery ...string) (bytes.Buffer, error) {
+// unit test를 하기 위해서는 function 내 다른 package function 호출, package 내 다른 function 호출을 unit test시 가짜 function으로 대체할 수 있게 mocking이 가능한 구조로 구현 필요
+// RunSSHCmd에서는 다른 package function인 exec.CommandContext를 호출하여 cmd를 받고, cmd.Run()을 통해 최종적으로 exec 실행
+// 다른 package의 function을 unit test시 대체하기 위해 호출할 function을 인자로 받아서 구조로 구현 필요
+// function에서 다른 package function 호출할 경우, interface를 인자로 받아 interface를 function으로 호출, interface 구현은 interface.go 참조
+func RunSSHCmd(exec ExecInterface, hostName, hostAddr string, cephQuery ...string) (bytes.Buffer, error) {
+	// exec interface  인자로 받음
 	var resultStdout, resultStderr bytes.Buffer
 
 	ctx, cancel := context.WithTimeout(context.Background(), cmdTimeout)
@@ -22,12 +26,8 @@ func RunSSHCmd(hostName, hostAddr string, cephQuery ...string) (bytes.Buffer, er
 	/// Add password option to sshCmd
 	sshCmd := []string{hostInfo}
 	sshCmd = append(sshCmd, cephQuery...)
-
-	cmd := exec.CommandContext(ctx, "ssh", sshCmd...)
-	cmd.Stdout = &resultStdout
-	cmd.Stderr = &resultStderr
-
-	err := cmd.Run()
+	// exec interface 의 function 호출
+	err := exec.commandExecute(&resultStdout, &resultStderr, ctx, "ssh", sshCmd...)
 
 	if err != nil {
 		return resultStderr, err
