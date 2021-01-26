0e8ff448cb580ac6806af240dd7bd83aa72ad17a
donggyu_park <donggyu_park@tmax.co.kr>
2021-01-26 11:50:11 +0900

    Add marshal function in yaml wrapper

--- pkg/common/wrapper/yaml_wrapper.go
+++ pkg/common/wrapper/yaml_wrapper.go
@@ -1,22 +1,26 @@
 package wrapper
 
 import (
-    "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
 )
 
 type YamlInterface interface {
-    Unmarshal(in []byte, out interface{}) (err error)
+	Unmarshal(in []byte, out interface{}) (err error)
+	Marshal(in interface{}) (out []byte, err error)
 }
 
 type yamlStruct struct {
 }
 
 func (y *yamlStruct) Unmarshal(in []byte, out interface{}) (err error) {
-    return yaml.Unmarshal(in, out)
+	return yaml.Unmarshal(in, out)
+}
+func (y *yamlStruct) Marshal(in interface{}) (out []byte, err error) {
+	return yaml.Marshal(in)
 }
 
 var YamlWrapper YamlInterface
 
 func init() {
-    YamlWrapper = &yamlStruct{}
+	YamlWrapper = &yamlStruct{}
 }
