d9aac51445cabf1b6330dd8235784395f155ccd4
SangHyeok-Kim95 <sanghyeok_kim@tmax.co.kr>
2021-01-29 10:31:21 +0900

    Add simple unit test - Add simple unit test code for Ceph spec read logic - Import github.com/tmax-cloud/hypersds-operator create-cm branch

--- pkg/util/ceph_spec.go
+++ pkg/util/ceph_spec.go
@@ -1,26 +1,27 @@
 package util
 
 import (
-    hypersdsv1alpha1 "github.com/tmax-cloud/hypersds-operator/api/v1alpha1"
-    "hypersds-provisioner/pkg/common/wrapper"
+	"hypersds-provisioner/pkg/common/wrapper"
+
+	hypersdsv1alpha1 "github.com/tmax-cloud/hypersds-operator/api/v1alpha1"
 )
 
 const (
-    FilePathPrefix = "/root/"
-    CephCrYamlName = "cluster.yaml"
+	FilePathPrefix = "/root/"
+	CephCrYamlName = "cluster.yaml"
 )
 
 func getCephClusterSpec(ioutil wrapper.IoUtilInterface, yaml wrapper.YamlInterface) (hypersdsv1alpha1.CephClusterSpec, error) {
-    fileName := FilePathPrefix + CephCrYamlName
-    source, err := ioutil.ReadFile(fileName)
-    if err != nil {
-        panic(err)
-    }
+	fileName := FilePathPrefix + CephCrYamlName
+	source, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
 
-    var t hypersdsv1alpha1.CephClusterSpec
-    err = yaml.Unmarshal(source, &t)
-    if err != nil {
-        panic(err)
-    }
+	var t hypersdsv1alpha1.CephClusterSpec
+	err = yaml.Unmarshal(source, &t)
+	if err != nil {
+		panic(err)
+	}
 	return t, nil
 }
