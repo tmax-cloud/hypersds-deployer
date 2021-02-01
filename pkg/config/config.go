68aca543da1c4868960cbd6b0a2c3942c9d580fd
Jinhong <hookak25@gmail.com>
2021-02-01 02:26:50 +0000

    Implement config class
    Manage ceph.conf for installing ceph cluster
    Update Configmap and Secret to kubernetes about ceph-cluster

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,184 @@
 package config
+
+import (
+	"context"
+	"fmt"
+	"hypersds-provisioner/pkg/common/wrapper"
+	"strings"
+
+	hypersdsv1alpha1 "github.com/tmax-cloud/hypersds-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type CephConfigInterface interface {
+	ConfigFromAdm(wrapper.IoUtilInterface, string) error
+	SecretFromAdm(wrapper.IoUtilInterface, string) error
+	MakeIni() string
+	UpdateConfToK8s(kubeWrapper wrapper.KubeInterface) error
+	UpdateKeyringToK8s(kubeWrapper wrapper.KubeInterface) error
+
+	SetCrConf(map[string]string) error
+	SetAdmConf(map[string]string) error
+	SetAdmSecret(map[string][]byte) error
+
+	GetCrConf() (map[string]string, error)
+	GetAdmConf() (map[string]string, error)
+	GetAdmSecret(map[string][]byte, error)
+}
+
+func (conf *CephConfig) SetCrConf(m map[string]string) error {
+	conf.crConf = m
+	return nil
+}
+
+func (conf *CephConfig) SetAdmConf(m map[string]string) error {
+	conf.admConf = m
+	return nil
+}
+
+func (conf *CephConfig) SetAdmSecret(m map[string][]byte) error {
+	conf.admSecret = m
+	return nil
+}
+
+func (conf *CephConfig) GetCrConf() (map[string]string, error) {
+	return conf.crConf, nil
+}
+
+func (conf *CephConfig) GetAdmConf() (map[string]string, error) {
+	return conf.admConf, nil
+}
+
+func (conf *CephConfig) GetAdmSecret() (map[string][]byte, error) {
+	return conf.admSecret, nil
+}
+
+type CephConfig struct {
+	crConf    map[string]string
+	admConf   map[string]string
+	admSecret map[string][]byte
+}
+
+type ConfigInitStruct struct{}
+
+func (c *ConfigInitStruct) NewConfigFromCephCr(cephCr hypersdsv1alpha1.CephClusterSpec) *CephConfig {
+	conf := CephConfig{}
+	crConf := make(map[string]string)
+
+	for key, value := range cephCr.Config {
+		crConf[key] = value
+	}
+
+	err := conf.SetCrConf(crConf)
+	if err != nil {
+		return nil
+	}
+	return &conf
+}
+
+func (conf *CephConfig) ConfigFromAdm(ioUtil wrapper.IoUtilInterface, cephconf string) error {
+	admConf := make(map[string]string)
+
+	dat, err := ioUtil.ReadFile(cephconf)
+	if err != nil {
+		return err
+	}
+
+	lines := strings.Split(string(dat[:]), "\n")
+	for _, s := range lines {
+		kv := strings.Split(s, "=")
+		if len(kv) > 1 {
+			key := strings.TrimSpace(kv[0])
+			val := strings.TrimSpace(kv[1])
+			admConf[key] = val
+		}
+	}
+	return conf.SetAdmConf(admConf)
+}
+
+func (conf *CephConfig) SecretFromAdm(ioUtil wrapper.IoUtilInterface, cephsecret string) error {
+	admSecret := make(map[string][]byte)
+
+	dat, err := ioUtil.ReadFile(cephsecret)
+	if err != nil {
+		return err
+	}
+
+	admSecret["keyring"] = dat
+	return conf.SetAdmSecret(admSecret)
+}
+
+func (conf *CephConfig) MakeIni() string {
+	ini := "[global]\n"
+	crConf, _ := conf.GetCrConf()
+	for key, value := range crConf {
+		s1 := fmt.Sprintf("\t%s = %s\n", key, value)
+		ini = fmt.Sprintf("%s%s", ini, s1)
+	}
+	return ini
+}
+
+//default:default SA가 configmap put할수 있는 rolebinding 있어야함!
+func (conf *CephConfig) UpdateConfToK8s(kubeWrapper wrapper.KubeInterface) error {
+	//creates the in-cluster config
+	config, err := kubeWrapper.InClusterConfig()
+	if err != nil {
+		return err
+	}
+
+	clientset, err := kubeWrapper.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	admConf, _ := conf.GetAdmConf()
+	configMap := corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "ceph-conf",
+		},
+		Data: admConf,
+	}
+	_, err = clientset.CoreV1().ConfigMaps("default").Get(context.TODO(), "ceph-conf", metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	_, err = clientset.CoreV1().ConfigMaps("default").Update(context.TODO(), &configMap, metav1.UpdateOptions{})
+	return err
+}
+
+func (conf *CephConfig) UpdateKeyringToK8s(kubeWrapper wrapper.KubeInterface) error {
+	//creates the in-cluster config
+	config, err := kubeWrapper.InClusterConfig()
+	if err != nil {
+		return err
+	}
+
+	clientset, err := kubeWrapper.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+
+	admSecret, _ := conf.GetAdmSecret()
+	secret := corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "ceph-secret",
+		},
+		Data: admSecret,
+	}
+
+	_, err = clientset.CoreV1().Secrets("default").Get(context.TODO(), "ceph-secret", metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	_, err = clientset.CoreV1().Secrets("default").Update(context.TODO(), &secret, metav1.UpdateOptions{})
+	return err
+}
---
+++ pkg/config/config_test.go
@@ -0,0 +1,138 @@
+package config
+
+import (
+	"hypersds-provisioner/pkg/common/wrapper"
+
+	gomock "github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/testing"
+)
+
+var _ = Describe("Config Test", func() {
+	defer GinkgoRecover()
+
+	var (
+		mockCtrl *gomock.Controller
+		kubeMock *wrapper.MockKubeInterface
+		ioMock   *wrapper.MockIoUtilInterface
+	)
+
+	BeforeEach(func() {
+		mockCtrl = gomock.NewController(GinkgoT())
+		kubeMock = wrapper.NewMockKubeInterface(mockCtrl)
+		ioMock = wrapper.NewMockIoUtilInterface(mockCtrl)
+	})
+
+	AfterEach(func() {
+		mockCtrl.Finish()
+	})
+
+	Describe("[ConfigFromAdm Test]", func() {
+		It("Parse ceph.conf to AdmConfig", func() {
+			ioMock.EXPECT().ReadFile(gomock.Any()).DoAndReturn(
+				func(filename string) ([]byte, error) {
+					conf := []byte("[global]\n\tfsid = b29fd\n\tmon_host = [0.0.0.0]")
+					return conf, nil
+				}).AnyTimes()
+			testConfig := CephConfig{}
+			AdmConfig := map[string]string{
+				"fsid":     "b29fd",
+				"mon_host": "[0.0.0.0]",
+			}
+			err := testConfig.ConfigFromAdm(ioMock, "ceph.conf")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(testConfig.GetAdmConf()).To(Equal(AdmConfig))
+		})
+	})
+
+	Describe("[SecretFromAdm Test]", func() {
+		It("Parse keyring to AdmSecret", func() {
+			ioMock.EXPECT().ReadFile(gomock.Any()).DoAndReturn(
+				func(filename string) ([]byte, error) {
+					secret := []byte("[client.admin]\n\tkey = b29fd")
+					return secret, nil
+				}).AnyTimes()
+			testConfig := CephConfig{}
+			AdmSecret := map[string][]byte{
+				"keyring": []byte("[client.admin]\n\tkey = b29fd"),
+			}
+			err := testConfig.SecretFromAdm(ioMock, "keyring")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(testConfig.GetAdmSecret()).To(Equal(AdmSecret))
+		})
+	})
+
+	Describe("[MakeIni Test]", func() {
+		It("Make Ini file from Map", func() {
+			testConfig := CephConfig{
+				crConf: map[string]string{
+					"debug_osd": "20/20",
+				},
+			}
+			ini := "[global]\n\tdebug_osd = 20/20\n"
+			retini := testConfig.MakeIni()
+			Expect(retini).To(Equal(ini))
+		})
+	})
+
+	Describe("[UpdateConfToK8s Test]", func() {
+		It("should return nil", func() {
+			kubeMock.EXPECT().InClusterConfig().Return(nil, nil).AnyTimes()
+
+			kubeMock.EXPECT().NewForConfig(gomock.Any()).DoAndReturn(
+				func(c interface{}) (kubernetes.Interface, error) {
+					fakeClient := fake.NewSimpleClientset()
+					fakeClient.PrependReactor("get", "configmaps", func(action testing.Action) (bool, runtime.Object, error) {
+						return true, nil, nil
+					})
+
+					fakeClient.PrependReactor("create", "configmaps", func(action testing.Action) (bool, runtime.Object, error) {
+						return true, nil, nil
+					})
+
+					fakeClient.PrependReactor("update", "configmaps", func(action testing.Action) (bool, runtime.Object, error) {
+						return true, nil, nil
+					})
+					return fakeClient, nil
+				}).AnyTimes()
+
+			testConfig := CephConfig{
+				crConf: map[string]string{
+					"test": "test",
+				},
+			}
+			err := testConfig.UpdateConfToK8s(kubeMock)
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+	Describe("[UpdateKeyringToK8s Test]", func() {
+		It("should return nil", func() {
+			kubeMock.EXPECT().InClusterConfig().Return(nil, nil).AnyTimes()
+
+			kubeMock.EXPECT().NewForConfig(gomock.Any()).DoAndReturn(
+				func(c interface{}) (kubernetes.Interface, error) {
+					fakeClient := fake.NewSimpleClientset()
+					fakeClient.PrependReactor("get", "secrets", func(action testing.Action) (bool, runtime.Object, error) {
+						return true, nil, nil
+					})
+					fakeClient.PrependReactor("update", "secrets", func(action testing.Action) (bool, runtime.Object, error) {
+						return true, nil, nil
+					})
+					return fakeClient, nil
+				}).AnyTimes()
+			testConfig := CephConfig{
+				admSecret: map[string][]byte{
+					"keyring": {0, 0, 0, 0},
+				},
+			}
+			err := testConfig.UpdateKeyringToK8s(kubeMock)
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+})
